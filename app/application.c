9a9767377ad3f16164c91bdecede3a597503f387
Robert Rongen <37987463+robertrongen@users.noreply.github.com>
2020-01-09 12:09:47 +0100

    modified:   app/application.c

--- app/application.c
+++ app/application.c
@@ -8,24 +8,16 @@ bc_led_t led;
 // Button instance
 bc_button_t button;
 
+// GFX instance
 bc_gfx_t *gfx;
 
 void qrcode_project(char *project_name);
 
-
+// QR code variables
 const char *str_urls[] =
 {
-    "https://www.hardwario.com/",
-    "https://www.hackster.io/hardwario",
-    "http://hub.local",
-    /*
-        "Alice was beginning to get very tired of sitting by her sister on the bank, "
-			"and of having nothing to do: once or twice she had peeped into the book her sister was reading, "
-			"but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice "
-			"'without pictures or conversations?' So she was considering in her own mind (as well as she could, "
-			"for the hot day made her feel very sleepy and stupid), whether the pleasure of making a "
-			"daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly "
-			"a White Rabbit with pink eyes ran close by her.",*/
+    "http://blokko.blockchainadvies.nu/receive-order.html?orderid=1",
+    "http://blokko.blockchainadvies.nu/receive-order.html",
     NULL
 };
 
@@ -54,14 +46,13 @@ static void print_qr(const uint8_t qrcode[])
     bc_gfx_set_font(gfx, &bc_font_ubuntu_13);
     bc_gfx_draw_string(gfx, 2, 0, (char*)str_urls[project_index], true);
 
-    uint32_t offset_x = 8;
-    uint32_t offset_y = 32;
-    uint32_t box_size = 2;
+    uint32_t offset_x = 15;
+    uint32_t offset_y = 16;
+    uint32_t box_size = 3;
 	int size = qrcodegen_getSize(qrcode);
-	int border = 4;
+	int border = 2;
 	for (int y = -border; y < size + border; y++) {
 		for (int x = -border; x < size + border; x++) {
-			//fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
             uint32_t x1 = offset_x + x * box_size;
             uint32_t y1 = offset_y + y * box_size;
             uint32_t x2 = x1 + box_size;
@@ -69,9 +60,7 @@ static void print_qr(const uint8_t qrcode[])
 
             bc_gfx_draw_fill_rectangle(gfx, x1, y1, x2, y2, qrcodegen_getModule(qrcode, x, y));
 		}
-		//fputs("\n", stdout);
 	}
-	//fputs("\n", stdout);
     bc_gfx_update(gfx);
 }
 
@@ -82,7 +71,7 @@ void qrcode_project(char *text)
 	// Make and print the QR Code symbol
 	static uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
 	static uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
-	bool ok = qrcodegen_encodeText(text, tempBuffer, qrcode, qrcodegen_Ecc_LOW,	qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+	bool ok = qrcodegen_encodeText(text, tempBuffer, qrcode, qrcodegen_Ecc_MEDIUM,	qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
 
 	if (ok)
     {
